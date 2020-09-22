b0dfc084d43fb453a6af933d08f800a2a50176b2
Joel Estrada <jre.estrada@gmail.com>
2020-09-21 22:51:15 -0700

    added functionality to view high scores and go back buttons

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,7 @@
 var h1Tag = document.createElement("h1");
 h1Tag.textContent = "Coding Quiz Challenge";
 h1Tag.style.textAlign = "center";
+h1Tag.style.marginTop = "75px";
 h1Tag.setAttribute("id", "title")
 
 document.body.appendChild(h1Tag);
@@ -20,6 +21,18 @@ p1Tag.style.fontWeight = "bold";
 
 document.body.appendChild(p1Tag);
 
+// Start Quiz button
+var startQuizButton = document.createElement("button");
+startQuizButton.textContent = "Start Quiz";
+startQuizButton.style.marginLeft = "auto";
+startQuizButton.style.marginRight = "auto";
+startQuizButton.style.display = "block";
+startQuizButton.style.backgroundColor = "rgb(117,92,150)";
+startQuizButton.style.color = "white";
+startQuizButton.setAttribute("id", "startQuiz")
+
+document.body.appendChild(startQuizButton);
+
 // View High Scores link at top left corner
 var viewHighScoresPTag = document.createElement("p");
 viewHighScoresPTag.textContent = "View High Scores";
@@ -29,7 +42,7 @@ viewHighScoresPTag.style.left = "0";
 viewHighScoresPTag.style.marginTop = "4px";
 viewHighScoresPTag.style.marginLeft = "4px";
 viewHighScoresPTag.style.fontWeight = "bold";
-viewHighScoresPTag.style.color = "purple";
+viewHighScoresPTag.style.color = "rgb(117,92,150)";
 viewHighScoresPTag.setAttribute("id", "viewHighScores");
 
 document.body.appendChild(viewHighScoresPTag);
@@ -47,11 +60,55 @@ timerPTag.style.fontWeight = "bold";
 
 document.body.appendChild(timerPTag);
 
+// create a div container for the buttons
+var buttonContainer = document.createElement("div");
+buttonContainer.style.textAlign = "center";
+buttonContainer.style.marginLeft = "auto";
+buttonContainer.style.marginRight = "auto";
+buttonContainer.style.display = "block";
+buttonContainer.setAttribute("id", "container")
+
+// Create Go Back button 
+var goBackButton = document.createElement("button");
+goBackButton.textContent = "Go Back";
+goBackButton.style.marginRight = "6px";
+goBackButton.style.backgroundColor = "rgb(117,92,150)";
+goBackButton.style.color = "white";
+goBackButton.setAttribute("id", "goBack")
+
+// Create Clear High Scores button 
+var clearHighScoresButton = document.createElement("button");
+clearHighScoresButton.textContent = "Clear High Scores";
+clearHighScoresButton.style.marginLeft = "6px";
+clearHighScoresButton.style.backgroundColor = "rgb(117,92,150)";
+clearHighScoresButton.style.color = "white";
+clearHighScoresButton.setAttribute("id", "clearHighScores")
+
 // when View High Scores is clicked, show different h1 title, show high scores, show an input form for the user to enter a high score, show a Go Back button that'll lead back to the starting page, show a Clear High Scores button that'll clear the high scores list 
 var clickViewHighScores = document.querySelector("#viewHighScores");
 clickViewHighScores.addEventListener("click", function () {
+    document.body.removeChild(timerPTag);
+    document.body.removeChild(viewHighScoresPTag);
+    document.body.removeChild(startQuizButton);
+    document.body.removeChild(p1Tag);
     title.textContent = "High Scores";
-    viewHighScoresPTag.textContent = "";
-    p1Tag.textContent = "";
-    timerPTag.textContent = "";
+
+    document.body.appendChild(buttonContainer);
+    buttonContainer.appendChild(goBackButton);
+    buttonContainer.appendChild(clearHighScoresButton);
+
+    var clickGoBack = document.querySelector("#goBack");
+    clickGoBack.addEventListener("click", goBackToStart);
 });
+
+function goBackToStart() {
+    buttonContainer.removeChild(clearHighScoresButton);
+    buttonContainer.removeChild(goBackButton);
+    document.body.removeChild(buttonContainer);
+
+    document.body.appendChild(p1Tag);
+    document.body.appendChild(startQuizButton);
+    document.body.appendChild(viewHighScoresPTag);
+    document.body.appendChild(timerPTag);
+    title.textContent = "Coding Quiz Challenge";
+}
